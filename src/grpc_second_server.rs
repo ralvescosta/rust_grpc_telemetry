b791f588ec614c722c362130675dadddf27a02f7
ralvescosta <rafael.rac.mg@gmail.com>
2021-11-08 06:22:09 -0300

    second server

---
+++ src/grpc_second_server.rs
@@ -0,0 +1,83 @@
+use opentelemetry::propagation::Extractor;
+use opentelemetry::{global, sdk::propagation::TraceContextPropagator};
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
+use tracing::*;
+use tracing_opentelemetry::OpenTelemetrySpanExt;
+use tracing_subscriber::prelude::*;
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld"); // The string specified here must match the proto package name
+}
+
+use hello_world::greeter_server::{Greeter, GreeterServer};
+use hello_world::{HelloReply, HelloRequest};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .with_service_name("grpc-second-server")
+        .install_batch(opentelemetry::runtime::Tokio)?;
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new("INFO"))
+        .with(tracing_opentelemetry::layer().with_tracer(tracer))
+        .try_init()?;
+
+    let addr = "[::1]:50052".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
+
+struct MetadataMap<'a>(&'a tonic::metadata::MetadataMap);
+impl<'a> Extractor for MetadataMap<'a> {
+    /// Get a value for a key from the MetadataMap.  If the value can't be converted to &str, returns None
+    fn get(&self, key: &str) -> Option<&str> {
+        self.0.get(key).and_then(|metadata| metadata.to_str().ok())
+    }
+
+    /// Collect all the keys from the MetadataMap.
+    fn keys(&self) -> Vec<&str> {
+        self.0
+            .keys()
+            .map(|key| match key {
+                tonic::metadata::KeyRef::Ascii(v) => v.as_str(),
+                tonic::metadata::KeyRef::Binary(v) => v.as_str(),
+            })
+            .collect::<Vec<_>>()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    #[instrument]
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<HelloReply>, Status> {
+        let parent_cx =
+            global::get_text_map_propagator(|prop| prop.extract(&MetadataMap(request.metadata())));
+        tracing::Span::current().set_parent(parent_cx);
+
+        let name = request.into_inner().name;
+
+        // Return an instance of type HelloReply
+        let reply = hello_world::HelloReply {
+            message: format!("Hello {}!", name), // We must use .into_inner() as the fields of gRPC requests and responses are private
+        };
+
+        println!("[SECOND SERVER] - Send response");
+
+        Ok(Response::new(reply)) // Send back our formatted greeting
+    }
+}
